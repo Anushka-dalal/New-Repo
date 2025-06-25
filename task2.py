88c37050d0c01a83d893835af750596c58c5c35a
Anushka-dalal <anushkadalal80@gmail.com>
2025-06-25 12:05:36 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,26 @@
+# hw task -> 
+# student has features like name, roll and subjects with marks. There are 5 subjects for every students with some marks.
+# student has one operation which will display percentage of student.
+# use dictionary for subject and marks
+
+class Student:
+    def __init__(self,nm, r, **sub):
+        self.name = nm
+        self.roll = r
+        self.subjects = sub
+
+    def display(self):
+        total = sum(self.subjects.values())
+        percent = (total* 100) / 500
+        print("Name of Student =", self.name)
+        print("Roll No of Student =", self.roll)
+        for s,m in self.subjects.items():
+            print(f"Marks in {s} = {m}")
+        print(f"Percentage of Student = {percent} %")            
+
+
+s = Student("Anushka",1, Math=90, Science=85, English=88, History=92, Geography=89)
+s.display()
+
+
+
