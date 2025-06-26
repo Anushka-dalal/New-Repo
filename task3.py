b638331c1219b4c362a047f1dd343c330615b92b
Anushka-dalal <anushkadalal80@gmail.com>
2025-06-26 11:57:08 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,14 @@
+from package1.my_decorator import my_decorator   #importing 'my_decorator' function 
+class Decorator:
+    def __init__(self,a,b):
+        self.num1 = a 
+        self.num2 = b
+          
+    @my_decorator
+    def sum(self):
+        sum = self.num1 + self.num2
+        print("Before modification =", sum)
+        return sum
+
+s = Decorator(10,20)
+s.sum()
