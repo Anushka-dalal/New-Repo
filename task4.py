1a59d94baeab3c7813186d66b6bd882d16d3a574
Anushka-dalal <anushkadalal80@gmail.com>
2025-06-30 22:46:53 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,39 @@
+# Hybrid Inheritance
+class A:
+    def m1(self):
+        print("Class A:Method m1")
+    
+class B(A):
+    def m2(self):
+        print("Class B:Method m2")
+
+class C(A):
+    def m3(self):
+        print("Class C:Method m3")
+
+class D:
+    def m4(self):
+        print("Class D:Method m4")
+    
+class E(B):
+    def m5(self):
+        print("Class E:Method m5")
+
+class F(C,D):
+    def m6(self):
+        print("Class F:Method m6")
+
+jay = E()
+viru = F()
+
+jay.m1()
+jay.m2()
+jay.m5()
+
+viru.m1()
+viru.m3()
+viru.m4()
+viru.m6()
+
+print(F.mro()) 
+print(E.mro())
